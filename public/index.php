9f7c0151dc5de93e977dc08c4ab384427e8584a7
Mischief <lukasz.marszalek@interia.pl>
2016-11-26 14:50:40 +0100

    Deprecated disable

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
  * to the application root now.
  */
 chdir(dirname(__DIR__));
-
+error_reporting(0);
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
